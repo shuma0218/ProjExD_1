7a6e17bb887eba70150b1367644f6b24748f6743
shuma <c0b220134e@edu.teu.ac.jp>
2024-11-19 15:37:04 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bgf_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct .center = 300,200
     tmr = 0
     x = 0
     xa = 0
@@ -20,13 +22,26 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [xa, 0])
-        x -= 100
-        xa = x % 1600
-        screen.blit(kk_img, [300,200])
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bgf_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bgf_img, [-x+4800, 0])
+        x += 1
+        if x == 4800:
+            x = 0
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,+1))
+        elif key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1,0))
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
