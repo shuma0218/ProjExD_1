0d2d7e1db82c0ea2c030647c5968fd5026766ccb
shuma <c0b220134e@edu.teu.ac.jp>
2024-11-19 15:08:43 +0900

    背景ループ

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bgf_img = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
+    x = 0
+    xa = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [xa, 0])
+        x -= 100
+        xa = x % 1600
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
